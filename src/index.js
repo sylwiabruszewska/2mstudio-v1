0badf60b5a8604e0ffb547a78d9d6d2b92b6405a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-04 15:37:04 +0100

    change project structure, add home page, routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { App } from './App';
+import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/2mstudio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
