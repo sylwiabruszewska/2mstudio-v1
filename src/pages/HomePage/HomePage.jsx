0239772a495a9eca43efdb5b2dd217ad132e16fa
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-04 18:28:01 +0100

    add hero image

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
 import { Helmet } from 'react-helmet';
 
+import styles from './HeroPage.module.scss';
+import { Container } from 'components';
+
 const Home = () => {
   return (
     <>
       <Helmet>
         <title>2m Studio Pracownia Projektowa</title>
       </Helmet>
+
+      <Container type="full">
+        <div className={styles['hero']}></div>
+      </Container>
 
       <p>Home</p>
     </>
