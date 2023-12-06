3550be5285e0f9281750c2af70ba44ad8a720461
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-06 16:53:07 +0100

    change slider library in SliderPortfolio

--- src/components/SliderPortfolio/SliderPortfolio.jsx
+++ src/components/SliderPortfolio/SliderPortfolio.jsx
@@ -1,7 +1,5 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import styles from './SliderPortfolio.module.scss';
 import image1 from '../../assets/images/slider/interior1.jpg';
@@ -10,88 +8,54 @@ import image3 from '../../assets/images/slider/interior3.jpg';
 import image4 from '../../assets/images/slider/interior4.jpg';
 
 export const SliderPortfolio = () => {
-  const PrevArrow = ({ onClick }) => (
-    <div className={styles['prev-arrow-custom']} onClick={onClick}>
-      <FaArrowLeft className={styles['arrow-icon']} />
-    </div>
-  );
-
-  const NextArrow = ({ onClick }) => (
-    <div className={styles['next-arrow-custom']} onClick={onClick}>
-      <FaArrowRight className={styles['arrow-icon']} />
-    </div>
-  );
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
   return (
-    <Slider {...settings}>
-      <div>
-        <img
-          src={image1}
-          className={styles['slider-img']}
-          alt="Opis obrazka 1"
-        />
-      </div>
-      <div>
-        <img
-          src={image2}
-          className={styles['slider-img']}
-          alt="Opis obrazka 2"
-        />
-      </div>
-      <div>
-        <img
-          src={image3}
-          className={styles['slider-img']}
-          alt="Opis obrazka 3"
-        />
-      </div>
-      <div>
-        <img
-          src={image4}
-          className={styles['slider-img']}
-          alt="Opis obrazka 4"
-        />
-      </div>
-      <div>
-        <img
-          src={image1}
-          className={styles['slider-img']}
-          alt="Opis obrazka 1"
-        />
-      </div>
-      <div>
-        <img
-          src={image2}
-          className={styles['slider-img']}
-          alt="Opis obrazka 2"
-        />
-      </div>
-    </Slider>
+    <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlay
+      autoPlaySpeed={3000}
+      centerMode={false}
+      containerClass=""
+      customButtonGroup={null}
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      keyBoardControl
+      minimumTouchDrag={80}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1280,
+          },
+          items: 3,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1280,
+            min: 768,
+          },
+          items: 2,
+        },
+        mobile: {
+          breakpoint: {
+            max: 768,
+            min: 0,
+          },
+          items: 1,
+        },
+      }}
+      sliderClass=""
+    >
+      <img src={image1} className={styles['slider-img']} alt="Opis obrazka 1" />
+      <img src={image2} className={styles['slider-img']} alt="Opis obrazka 2" />
+      <img src={image3} className={styles['slider-img']} alt="Opis obrazka 3" />
+      <img src={image4} className={styles['slider-img']} alt="Opis obrazka 4" />
+      <img src={image1} className={styles['slider-img']} alt="Opis obrazka 1" />
+      <img src={image2} className={styles['slider-img']} alt="Opis obrazka 2" />
+    </Carousel>
   );
 };
