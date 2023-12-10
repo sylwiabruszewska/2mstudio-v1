074fc9428ac5deb218d7ba2669e2f3027ea62c38
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 16:36:07 +0100

    update AboutUsPage, add styles

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,13 +1,42 @@
 import { Helmet } from 'react-helmet';
+import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
+
+import styles from './AboutUsPage.module.scss';
+import { Section, Container } from 'components';
+import { getAboutUsInfo } from '../../services/api';
 
 const AboutUsPage = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAboutUsInfo();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>2m Studio Pracownia Projektowa - Portfolio - O nas</title>
       </Helmet>
 
-      <p>About Us</p>
+      <Section>
+        <Container>
+          <div className={styles['wrapper']}>
+            <div className={styles['content']}>
+              {data && parse(data.content.rendered)}
+            </div>
+          </div>
+        </Container>
+      </Section>
     </>
   );
 };
