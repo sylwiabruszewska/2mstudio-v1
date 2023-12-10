034a7f172cefd59c223b40697edaa396ca5b4e0c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 16:55:05 +0100

    update Interiors

--- src/pages/ProjectsPages/Interiors.jsx
+++ src/pages/ProjectsPages/Interiors.jsx
@@ -1,13 +1,72 @@
 import { Helmet } from 'react-helmet';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import styles from './Projects.module.scss';
+import { getImages } from '../../helpers/getImages';
+import { getPortfolioInterior } from '../../services/api';
+import { setIsLoading } from '../../redux/global/globalSlice';
 
 const Interiors = () => {
+  const [projects, setProjects] = useState();
+  const [featuredImages, setFeaturedImages] = useState({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(setIsLoading(true));
+      try {
+        const data = await getPortfolioInterior();
+        setProjects(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        dispatch(setIsLoading(false));
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  useEffect(() => {
+    getImages(projects, featuredImages, setFeaturedImages, getImages);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projects]);
+
   return (
     <>
       <Helmet>
         <title>2m Studio Pracownia Projektowa - Budynki przemysłowe</title>
       </Helmet>
 
-      <p>Interiors</p>
+      <ul className={styles['gallery']}>
+        {projects &&
+          projects.map(post => (
+            <li key={post.id} className={styles['photo-card']}>
+              <Link to={`/${post.id}`}>
+                {post.featured_media !== 0 &&
+                  featuredImages[post.featured_media] && (
+                    <div className={styles['img-container']}>
+                      <img
+                        className={styles['card__img']}
+                        src={featuredImages[post.featured_media]}
+                        alt={post.title.rendered}
+                        loading="lazy"
+                      />
+                      <div className={styles['card__overlay']}>
+                        <div className={styles['box']}>
+                          <p className={styles['card__title']}>
+                            {post.title.rendered}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+              </Link>
+            </li>
+          ))}
+      </ul>
     </>
   );
 };
