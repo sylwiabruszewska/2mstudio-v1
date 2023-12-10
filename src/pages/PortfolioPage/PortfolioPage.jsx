25f00fd805f5613d7c364ac8e7e7b17abff8d7ed
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 16:36:49 +0100

    add button group for PortfolioPage

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -1,6 +1,10 @@
 import { Helmet } from 'react-helmet';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { Container } from 'components';
+import styles from './PortfolioPage.module.scss';
 
 const PortfolioPage = () => {
   return (
@@ -9,11 +13,29 @@ const PortfolioPage = () => {
         <title>2m Studio Pracownia Projektowa - Portfolio</title>
       </Helmet>
 
-      <p>Portfolio</p>
+      <Container>
+        <div className={styles['btn-group']}>
+          <Link to="wnetrza">
+            <button className={styles['btn-iso']}>Wnętrza</button>
+          </Link>
 
-      <Suspense fallback={'is loading'}>
-        <Outlet />
-      </Suspense>
+          <Link to="budynki-mieszkalne">
+            <button className={styles['btn-iso']}>Budynki mieszkalne</button>
+          </Link>
+
+          <Link to="budynki-uslugowe">
+            <button className={styles['btn-iso']}>Budynki usługowe</button>
+          </Link>
+
+          <Link to="budynki-przemyslowe">
+            <button className={styles['btn-iso']}>Budynki przemysłowe</button>
+          </Link>
+        </div>
+
+        <Suspense fallback={''}>
+          <Outlet />
+        </Suspense>
+      </Container>
     </>
   );
 };
