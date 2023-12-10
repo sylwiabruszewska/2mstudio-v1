91e0b43f3bec014e65b661cdd0cf92031df917a0
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 02:15:45 +0100

    update layour - add loader

--- src/layout/SharedLayout.jsx
+++ src/layout/SharedLayout.jsx
@@ -1,24 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './SharedLayout.module.scss';
-import { Bar, Header, Footer } from 'components';
+import { Bar, Header, Footer, Loader } from 'components';
+import { selectIsLoading } from '../redux/global/selectors';
 
 export const SharedLayout = () => {
+  const isLoading = useSelector(selectIsLoading);
+
   return (
-    <div className={styles['wrapper']}>
-      <header>
-        <Bar />
-        <Header />
-      </header>
-      <main className={styles['page-main']}>
-        <Suspense fallback={'is loading'}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+    <>
+      {isLoading && <Loader />}
+      <div className={styles['wrapper']}>
+        <header>
+          <Bar />
+          <Header />
+        </header>
+        <main className={styles['page-main']}>
+          <Suspense fallback={'is loading'}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 };
