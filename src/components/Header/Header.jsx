8a47ec88c1c9cbc8c8e4c2444969a565e43db7c7
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-14 16:38:59 +0100

    add mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,22 @@ import { Link } from 'react-router-dom';
 
 import { StyledLink } from './Header.styled';
 import styles from './Header.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { IoMdClose } from 'react-icons/io';
+import { setIsMobileMenuOpen } from '../../redux/global/globalSlice';
+import { selectIsMobileMenuOpen } from '../../redux/global/selectors';
 
 export const Header = () => {
+  const dispatch = useDispatch();
+  const isMobileMenuVisible = useSelector(selectIsMobileMenuOpen);
+
+  const toggleMobileMenu = () => {
+    dispatch(setIsMobileMenuOpen(!isMobileMenuVisible));
+  };
+
   return (
     <div className={styles['page-header']}>
       <Link to="/" aria-label="strona główna">
@@ -36,9 +47,45 @@ export const Header = () => {
         className={styles['menu-toggle']}
         aria-expanded="false"
         aria-controls="mobile-menu"
+        onClick={toggleMobileMenu}
       >
         <AiOutlineMenu />
       </button>
+
+      {isMobileMenuVisible && (
+        <div className={styles['mobile-menu']}>
+          <button onClick={toggleMobileMenu} className={styles['close-button']}>
+            <IoMdClose />
+          </button>
+          <StyledLink
+            to="/"
+            aria-label="strona główna"
+            onClick={toggleMobileMenu}
+          >
+            Home
+          </StyledLink>
+          <StyledLink to="/blog" aria-label="blog" onClick={toggleMobileMenu}>
+            Blog
+          </StyledLink>
+          <StyledLink
+            to="/projekty/wnetrza"
+            aria-label="projekty"
+            onClick={toggleMobileMenu}
+          >
+            Projekty
+          </StyledLink>
+          <StyledLink to="/o-nas" aria-label="o nas" onClick={toggleMobileMenu}>
+            O nas
+          </StyledLink>
+          <StyledLink
+            to="/kontakt"
+            aria-label="kontakt"
+            onClick={toggleMobileMenu}
+          >
+            Kontakt
+          </StyledLink>
+        </div>
+      )}
     </div>
   );
 };
