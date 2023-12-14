208a1ed7a976744bebb8c58dc2c8eac0b8b30e2e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-14 22:43:52 +0100

    update gitignore

---
+++ src/config.js
@@ -0,0 +1,13 @@
+// config.js
+export const BASE_URL = 'https://www.2mstudio.org.pl/wordpress/wp-json/wp/v2/';
+
+export const API_PATHS = {
+  post: 'posts',
+  blog: 'posts?categories=5',
+  portfolioInterior: 'posts?categories=16',
+  portoflioBuildingsResidential: 'posts?categories=17',
+  portoflioBuildingsIndustrial: 'posts?categories=18',
+  portoflioBuildingsCommercial: 'posts?categories=19',
+  media: 'media/',
+  aboutUs: 'pages/112',
+};
