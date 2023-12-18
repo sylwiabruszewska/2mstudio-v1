973ac81f96a5053d2d6a960a374366c4a93f6963
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-18 12:57:09 +0100

    add loading indicator for AboutUsPage

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,21 +1,27 @@
 import { Helmet } from 'react-helmet';
 import { useEffect, useState } from 'react';
 import parse from 'html-react-parser';
+import { useDispatch } from 'react-redux';
 
 import styles from './AboutUsPage.module.scss';
 import { Section, Container } from 'components';
 import { getAboutUsInfo } from '../../services/api';
+import { setIsLoading } from '../../redux/global/globalSlice';
 
 const AboutUsPage = () => {
   const [data, setData] = useState();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
+      dispatch(setIsLoading(true));
       try {
         const data = await getAboutUsInfo();
         setData(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        dispatch(setIsLoading(false));
       }
     };
 
