317425b8e6a30b489dedbb8d282aa0e4494f3f36
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-03 23:56:55 +0100

    update browserrouter basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/2mstudio">
+      <BrowserRouter basename="/2mstudio-v1">
         <App />
       </BrowserRouter>
     </Provider>
